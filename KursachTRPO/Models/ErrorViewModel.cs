a552d0e107234eeba569930fdc5fdb329213a71d
Максим Мазур <maksim290698@mail.ru>
2019-02-23 21:31:38 +0300

    Добавьте файлы проекта.

---
+++ KursachTRPO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KursachTRPO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
